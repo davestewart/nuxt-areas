174b6f81ae24ffea3a857263a93ae9fbee56de6a
Dave Stewart <dave@davestewart.co.uk>
2021-11-16 13:27:21 +0000

    Simplify Vue 2 plugin code

--- src/plugins/vue-2.js
+++ src/plugins/vue-2.js
@@ -8,31 +8,28 @@ const stores = [
 
 // plugin function
 export default async function ({ store }) {
-  stores.forEach(info =>{
-    registerModule(store, info.namespace, info.module)
-  })
-}
+  // add all stores to vuex
+  for (const info of stores) {
+    // get namespace in vuex format
+    const namespace = info.namespace.split('/')
 
-// helper function
-function registerModule (store, namespace, module) {
-  // split
-  namespace = namespace.split('/')
-
-  // ensure parent namespaces have been registered
-  if (namespace.length > 1) {
-    for (let i = 0; i < namespace.length - 1; i++) {
-      const moduleName = namespace.slice(0, i + 1)
-      if (!store.hasModule(moduleName)) {
-        store.registerModule(moduleName, { namespaced: true, modules: {} })
+    // ensure parent namespaces have been registered
+    if (namespace.length > 1) {
+      for (let i = 0; i < namespace.length - 1; i++) {
+        const moduleName = namespace.slice(0, i + 1)
+        if (!store.hasModule(moduleName)) {
+          store.registerModule(moduleName, { namespaced: true, modules: {} })
+        }
       }
     }
-  }
 
-  // register module
-  // also @see https://github.com/nuxt/nuxt.js/issues/2267
-  store.registerModule(namespace, {
-    namespaced: true,
-    modules: {},
-    ...(module.default || module || {})
-  })
+    // register module
+    // also @see https://github.com/nuxt/nuxt.js/issues/2267
+    const module = info.module
+    store.registerModule(namespace, {
+      namespaced: true,
+      modules: {},
+      ...(module.default || module || {})
+    })
+  }
 }
